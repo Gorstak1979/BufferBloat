f13648e1b749f6e804ba0434e7b039d1692c8a23
Gorstak1979 <znastidobrostoje@gmail.com>
2025-12-12 10:06:19 -0800

    Add files via upload

---
+++ Bufferbloat.cmd
@@ -0,0 +1,9 @@
+@echo off
+Title Bufferbloat && Color 0b
+
+:: Step 1: Elevate
+>nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
+DEL /F /Q "%temp%\uac.vbs"
+
+:: Step 2: Bufferbloat
+netsh int tcp set global autotuninglevel=disabled
